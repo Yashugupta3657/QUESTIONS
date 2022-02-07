f69f6fdcaaea03de442638af03151aa5b90b2c55
Yashugupta3657 <60153103+Yashugupta3657@users.noreply.github.com>
2022-02-08 03:12:44 +0530

    Create Add"1"ToList.java

---
+++ LinkedLists/Add"1"ToList.java
@@ -0,0 +1,80 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+class GfG
+{
+    public static void printList(Node node) 
+    { 
+        while (node != null)
+        { 
+            System.out.print(node.data);
+            node = node.next; 
+        }  
+        System.out.println();
+    } 
+    public static void main(String args[])throws IOException
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Node head = new Node( s.charAt(0) - '0' );
+                    Node tail = head;
+                    for(int i=1; i<s.length(); i++)
+                    {
+                        tail.next = new Node( s.charAt(i) - '0' );
+                        tail = tail.next;
+                    }
+                    Solution obj = new Solution();
+                    head = obj.addOne(head);
+                    printList(head); 
+                }
+        }
+}// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+} 
+*/
+
+class Solution
+{
+    public static int call(Node head){
+        if(head==null){
+            return 1;
+        }
+        int sum=head.data+call(head.next);
+        head.data=sum%10;
+        return sum/10;
+    }
+    public static Node addOne(Node head) 
+    { 
+        int ree=call(head);
+        if(ree>0){  ///if number=99 it should become 100
+            Node n=new Node(ree);
+            n.next=head;
+            head=n;
+        }
+        return head;
+    }
+}
