ffcf3e7abd1b6b5845aed9ae2a0b140f1df80abd
Yashugupta3657 <60153103+Yashugupta3657@users.noreply.github.com>
2022-02-09 18:05:49 +0530

    Create MiddleOfLinkedList.java

---
+++ LinkedLists/MiddleOfLinkedList.java
@@ -0,0 +1,12 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next!=null&&fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+     
+        return slow;
+    }
+}
