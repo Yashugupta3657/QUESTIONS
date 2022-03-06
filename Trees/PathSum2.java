15834e471edb4030f68ea4ad4fc6338f6a178f9e
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-03-07 00:08:23 +0530

    Update PathSum2.java

--- Trees/PathSum2.java
+++ Trees/PathSum2.java
@@ -27,7 +27,7 @@ class Solution {
         if(root.left==null&&root.right==null){
             if(currs+root.val==targetsum){
                 part.add(root.val);
-                sol.add(new ArrayList<>(part));
+                sol.add(new ArrayList<>(part));   //we have to pass new copy because if we pass refrence it will change because of backtracking we are doing
                 part.remove(part.size()-1);
                 return;
             }
@@ -35,6 +35,6 @@ class Solution {
         part.add(root.val);
         path(root.left,targetsum,currs+root.val,part);
         path(root.right,targetsum,currs+root.val,part);
-        part.remove(part.size()-1);
+        part.remove(part.size()-1);            //list will not change because it is in heap so we have to manually remove to get previous state of list.
     }
 }
