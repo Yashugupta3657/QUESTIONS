fda1b1dd8f3ac77d241f5b9c799134ef08238851
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-03-08 21:22:44 +0530

    Update Inorder.java

--- Trees/Inorder.java
+++ Trees/Inorder.java
@@ -13,6 +13,48 @@
  *     }
  * }
  */
+//iterative way==>>
+class Check{
+    TreeNode curr;
+    boolean isleft;
+    boolean isright;
+    boolean isself;
+    public Check(TreeNode curr){
+        this.curr=curr;
+    }
+}
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> done=new ArrayList<>();
+        if(root==null)
+            return done;
+        Stack<Check> st=new Stack<>();
+        st.push(new Check(root));
+        while(!st.isEmpty()){
+            Check front=st.peek();
+            if(!front.isleft){
+                 if(front.curr.left!=null)
+                     st.push(new Check(front.curr.left));
+                front.isleft=true;
+            }
+             else if(!front.isself){
+                if(front.curr!=null)
+                done.add(front.curr.val);
+                front.isself=true;
+            }
+            else if(!front.isright){
+                if(front.curr.right!=null)
+                st.push(new Check(front.curr.right));
+                front.isright=true;
+            }
+            else{
+                st.pop();
+            }
+        }
+        return done;
+    }
+}
+//recursive way=>>>
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> m=new ArrayList<>();
