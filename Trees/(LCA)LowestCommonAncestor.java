552b3b620d27d57e3cf48c90e97320bbfb02f316
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-03-12 14:44:16 +0530

    Create (LCA)LowestCommonAncestor.java

---
+++ Trees/(LCA)LowestCommonAncestor.java
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return find(root,p,q);
+    }
+    public TreeNode find(TreeNode root,TreeNode p,TreeNode q){
+        if(root==null){
+            return null;
+        }
+        if(root.val==p.val||root.val==q.val){
+            return root;
+        }
+        TreeNode left=find(root.left,p,q);
+        TreeNode right=find(root.right,p,q);
+        if(left!=null&&right!=null){
+            return root;
+        }
+        else if(left!=null&&right==null)
+            return left;
+        else
+            return right;
+    }
+}
