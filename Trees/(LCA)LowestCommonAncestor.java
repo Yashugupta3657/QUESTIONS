4d674bd173dbef3e73bf8d61c71a662d3d3f4aa1
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-03-12 15:39:53 +0530

    Update (LCA)LowestCommonAncestor.java

--- Trees/(LCA)LowestCommonAncestor.java
+++ Trees/(LCA)LowestCommonAncestor.java
@@ -9,6 +9,42 @@
  * }
  */
 class Solution {
+    Boolean pexist;
+    Boolean qexist;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode ans=find(root,p,q);
+        if(pexist&&qexist)
+            return ans;
+        return null;
+    }
+    public TreeNode find(TreeNode root,TreeNode p,TreeNode q){
+        if(root==null){
+            return null;
+        }
+        TreeNode left=find(root.left,p,q);
+        TreeNode right=find(root.right,p,q);
+        if(root.val==p.val||root.val==q.val){
+            if(root.val==p.val){
+                pexist=true;
+            }
+            else qexist=true;
+            return root;
+        }
+        if(left==null&&right==null){
+            return right;
+        }
+        else if(left==null&&right!=null){
+            return right;
+        }
+        else if(left!=null&&right==null){
+            return left;
+        }
+        else return root;
+    }
+}
+
+//this solution was valid because in constraints it was written that p and q always exist it will not give correct ans if it wasn't mention another method exists too for that.
+/*class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         return find(root,p,q);
     }
@@ -29,4 +65,4 @@ class Solution {
         else
             return right;
     }
-}
+}*/
