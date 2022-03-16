5d790f60dbe9f080451baf3d46c4b92c7a3fba23
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-03-16 09:31:51 +0530

    Update RemoveDuplicatesSorted.java

--- LinkedLists/RemoveDuplicatesSorted.java
+++ LinkedLists/RemoveDuplicatesSorted.java
@@ -1,108 +1,39 @@
-// { Driver Code Starts
-/* package whatever; // don't place package name! */
-import java.util.*;
-class Node
-    {
-        int data;
-        Node next;
-        Node(int d) {data = d; next = null; }
-    }
-class Remove_Duplicate_From_LL
-{
-    Node head;  
-    Node tail;
-	public void addToTheLast(Node node) 
-	{
-	  if (head == null) 
-	  {
-	   head = node;
-	   tail = node;
-	  } 
-	  else 
-	  {
-	   tail.next = node;
-	   tail = node;
-	  }
-	}
-      void printList()
-    {
-        Node temp = head;
-        while (temp != null)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode n=head;
+        if(head==null)
         {
-           System.out.print(temp.data+" ");
-           temp = temp.next;
-        }  
-        System.out.println();
-    }
-	/* Drier program to test above functions */
-	public static void main(String args[])
-    {
-         Scanner sc = new Scanner(System.in);
-		 int t=sc.nextInt();
-		 while(t>0)
-         {
-			int n = sc.nextInt();
-			Remove_Duplicate_From_LL llist = new Remove_Duplicate_From_LL(); 
-			int a1=sc.nextInt();
-			Node head= new Node(a1);
-            llist.addToTheLast(head);
-            for (int i = 1; i < n; i++) 
-			{
-				int a = sc.nextInt(); 
-				llist.addToTheLast(new Node(a));
-			}
-			
-        GfG g = new GfG();
-		llist.head = g.removeDuplicates(llist.head);
-		llist.printList();
-		
-        t--;		
+            return head;
         }
-    }}// } Driver Code Ends
-
-
-/*
-class Node
-    {
-        int data;
-        Node next;
-        Node(int d) {data = d; next = null; }
-    }
-*/
-
-class GfG
-{
-    //Function to remove duplicates from sorted linked list.
-    Node removeDuplicates(Node head)
-    {
-	// Your code here	
-	
-	Node temp=head;
-	while(temp.next!=null){
-	    if(temp.data==temp.next.data){
-	        temp.next=temp.next.next;
-	    }
-	    else{
-	        temp=temp.next;
-	    }
-	}
-	return head;
-	/*
-	HashSet<Integer> mm=new HashSet<Integer>();
-	Node temp=head;
-	Node fi=new Node(head.data);
-	Node move=fi;
-	mm.add(head.data);
-	while(temp!=null){
-	    if(mm.contains(temp.data)==false){
-	          move.next=temp;
-	          move=move.next;
-	          move.next=null;
-	            mm.add(temp.data);
-	    }
-	    temp=temp.next;
-	}
-	return fi;
-   */
+        while(n!=null&&n.next!=null){
+            ListNode start=n;
+            if(n.val==n.next.val){
+                  while(n.next!=null&&n.val==n.next.val){
+                   n=n.next;
+               }
+                head=n.next;
+                n=head;
+            }
+            else if(n.next.next!=null&&n.next.val==n.next.next.val){
+               while(n.next.next!=null&&n.next.val==n.next.next.val){
+                   n=n.next;
+               } 
+                start.next=n.next.next;
+                n=start;
+            }
+            else
+            n=n.next;
+        }
+        return head;
     }
 }
