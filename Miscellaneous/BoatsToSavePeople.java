2ce2f8584c707e83b2655b4acb5b1cfaa75d0224
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-03-25 23:18:03 +0530

    Create BoatsToSavePeople.java

---
+++ Miscellaneous/BoatsToSavePeople.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/boats-to-save-people/submissions/
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int count=0;
+        int i=0;
+        int j=people.length-1;
+        while(i<=j){
+            if(people[i]+people[j]<=limit){
+                 count++;
+                i++;
+                j--;
+            }
+            else {
+                count++;
+                j--;
+            }
+        }
+        return count;
+    }
+}
